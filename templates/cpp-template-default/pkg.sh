b7bdcce2e6d99c70de884df4e54794ce7b9ef6a6
Jinghui Hu <hujinghui@buaa.edu.cn>
2026-02-22 13:20:05 +0800

    add: 添加构建脚本

--- templates/cpp-template-default/pkg.sh
+++ templates/cpp-template-default/pkg.sh
@@ -16,7 +16,3 @@ cmake --build output --config Release
 
 mkdir -p dist
 mv output/bin/cc04-physics-engine/Release/cc04-physics-engine.app dist
-ln -s /Applications dist/Applications
-hdiutil create -volname ${DMG_VOL} -srcfolder dist -ov -format UDZO ${DMG_IMG}
-mkdir -p ${DST_DIR}
-mv ${DMG_IMG} ${DST_DIR}
