0e6aea4403c8dda7ec174cfd85eccb90fa98a8de
Jinghui Hu <hujinghui@buaa.edu.cn>
2026-02-22 13:46:24 +0800

    add: 添加构建脚本

--- templates/cpp-template-default/pkg.sh
+++ templates/cpp-template-default/pkg.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+set -eux
+
+GIT_TAG=$(git describe --tags --always --dirty="+dev")
+OUT_DIR=output
+DST_DIR=dist
+OS_NAME=$(uname -s)
+OS_ARCH=$(uname -m)
+DMG_DIR=${OUT_DIR}/dmg
+
+APP_PKG=cc04-physics-engine
+DMG_VOL=${APP_PKG}-${OS_NAME}-${OS_ARCH}-${GIT_TAG}
+DMG_IMG=${OUT_DIR}/${APP_PKG}-${OS_NAME}-${OS_ARCH}-${GIT_TAG}.dmg
 
 # Check if cmake is installed
 if ! command -v cmake &> /dev/null
@@ -14,5 +26,9 @@ cmake -B output -G"Xcode" --log-level=STATUS
 
 cmake --build output --config Release
 
-mkdir -p dist
-mv output/bin/cc04-physics-engine/Release/cc04-physics-engine.app dist
+mkdir -p ${DMG_DIR}
+mv ${OUT_DIR}/bin/${APP_PKG}/Release/${APP_PKG}.app ${DMG_DIR}
+ln -s /Applications ${DMG_DIR}/Applications
+hdiutil create -volname ${DMG_VOL} -srcfolder ${DMG_DIR} -ov -format UDZO ${DMG_IMG}
+mkdir -p ${DST_DIR}
+mv ${DMG_IMG} ${DST_DIR}
