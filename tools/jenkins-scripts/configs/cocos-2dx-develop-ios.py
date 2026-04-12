8d4c9df6cf5f106aa21f1c5ab215b4e903d9d287
Jinghui Hu <hujinghui@buaa.edu.cn>
2026-04-12 16:43:37 +0800

    chore(jenkins-scripts): migrate remaining Python 2 print statements to Python 3 syntax
    - Convert print statements to print() function calls in config files (Android, iOS, Win32, Doxygen)
    - Update socket communication to use bytes literals (b'...') for network data transmission
    - Add traceback import to autotest.py for improved error handling
    - Fix string escaping in xcodebuild commands (backslash escaping)
    - Remove trailing whitespace and normalize formatting across all modified files
    - Ensure consistent Python 3 compatibility across build configuration and automation scripts

--- tools/jenkins-scripts/configs/cocos-2dx-develop-ios.py
+++ tools/jenkins-scripts/configs/cocos-2dx-develop-ios.py
@@ -1,8 +1,8 @@
 import os
-print 'Build Config:'
-print '    Host:MAC'
-print '    Branch:develop'
-print '    Target:iOS'
+print('Build Config:')
+print('    Host:MAC')
+print('    Branch:develop')
+print('    Target:iOS')
 if(os.path.exists('tools/jenkins-scripts/ios-build.sh') == False):
     node_name = os.environ['NODE_NAME']
     source_dir = '../cocos-2dx-develop-base-repo/node/' + node_name + "/."
@@ -12,8 +12,8 @@ if(os.path.exists('tools/jenkins-scripts/ios-build.sh') == False):
 os.system('git submodule update --init --force')
 ret = os.system('tools/jenkins-scripts/ios-build.sh')
 os.system('git clean -xdf -f')
-print 'build exit'
-print ret
+print('build exit')
+print(ret)
 if ret == 0:
     exit(0)
 else:
--- tools/jenkins-scripts/configs/cocos-2dx-doxygen.py
+++ tools/jenkins-scripts/configs/cocos-2dx-doxygen.py
@@ -1,9 +1,9 @@
 import os
 import platform
-print 'Build Config:'
-print '    Branch:develop'
-print '    Target:Doxygen'
-print '    doxygen doxygen.config'
+print('Build Config:')
+print('    Branch:develop')
+print('    Target:Doxygen')
+print('    doxygen doxygen.config')
 if(os.path.exists('docs/doxygen.config') == False):
     node_name = os.environ['NODE_NAME']
     source_dir = '../cocos-2dx-develop-base-repo/node/' + node_name
@@ -16,8 +16,8 @@ os.chdir('docs/')
 ret = os.system('doxygen doxygen.config')
 os.chdir('../')
 os.system('git clean -xdf -f')
-print 'build exit'
-print ret
+print('build exit')
+print(ret)
 if ret == 0:
     exit(0)
 else:
